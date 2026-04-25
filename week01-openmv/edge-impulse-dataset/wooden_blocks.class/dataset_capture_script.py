1d7e2e84df350189030e3dea7ed74afdeae02f7b
Deepen <deepen.mantri@gmail.com>
2026-04-25 17:12:06 +0000

    Add Edge Impulse FOMO model, labels and inference script

---
+++ week01-openmv/edge-impulse-dataset/wooden_blocks.class/dataset_capture_script.py
@@ -0,0 +1,27 @@
+# Dataset Capture Script - By: mdeepen - Sat Apr 25 2026
+
+# Use this script to control how your OpenMV Cam captures images for your dataset.
+# You should apply the same image pre-processing steps you expect to run on images
+# that you will feed to your model during run-time.
+
+import sensor
+import image
+import time
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)   # Modify as you like.
+sensor.set_framesize(sensor.VGA)   # Modify as you like.
+sensor.skip_frames(time=2000)
+
+clock = time.clock()
+
+while (True):
+    clock.tick()
+    img = sensor.snapshot()
+    # Apply lens correction if you need it.
+    # img.lens_corr()
+    # Apply rotation correction if you need it.
+    # img.rotation_corr()
+    # Apply other filters...
+    # E.g. mean/median/mode/midpoint/etc.
+    print(clock.fps())
